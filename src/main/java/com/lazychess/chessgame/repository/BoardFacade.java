8ba15f0a877fdf79636e12286e876670662c0078
Tom D <toms.druvaskalns@modulrfinance.com>
2023-02-12 13:41:15 +0000

    added facade for persisting

---
+++ src/main/java/com/lazychess/chessgame/repository/BoardFacade.java
@@ -0,0 +1,24 @@
+package com.lazychess.chessgame.repository;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lazychess.chessgame.chessgame.Board;
+import com.lazychess.chessgame.repository.entity.BoardDao;
+import com.lazychess.chessgame.repository.mapper.BoardDaoMapper;
+
+public class BoardFacade {
+    private final BoardRepository boardRepository;
+    private final BoardDaoMapper boardDaoMapper;
+
+    public BoardFacade(BoardRepository boardRepository, BoardDaoMapper boardDaoMapper) {
+        this.boardRepository = boardRepository;
+        this.boardDaoMapper = boardDaoMapper;
+    }
+
+    @Transactional
+    public Long persistBoard(Board board) {
+        BoardDao boardDao = boardDaoMapper.fromBoardObject(board);
+        boardDao = boardRepository.saveAndFlush(boardDao);
+        return boardDao.getId();
+    }
+}
--- src/test/java/com/lazychess/chessgame/repository/BoardDaoRepositoryIntegrationTest.java
+++ src/test/java/com/lazychess/chessgame/repository/BoardDaoRepositoryIntegrationTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,8 +14,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.lazychess.chessgame.repository.entity.BoardDao;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest(showSql = true)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ImportAutoConfiguration(classes = SecurityAutoConfiguration.class)
+@DataJpaTest(properties = "spring.main.web-application-type=servlet")
 public class BoardDaoRepositoryIntegrationTest {
 
     @Autowired

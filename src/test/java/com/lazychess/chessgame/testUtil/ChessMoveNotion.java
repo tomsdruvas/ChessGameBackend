2ba237a67840fbd5b5296e4bedaee3c9d0b2b7ac
Tom D <toms.druvaskalns@modulrfinance.com>
2023-03-22 22:54:36 +0000

    chess notion parser finished

--- src/test/java/com/lazychess/chessgame/testUtil/ChessMoveNotion.java
+++ src/test/java/com/lazychess/chessgame/testUtil/ChessMoveNotion.java
@@ -11,16 +11,73 @@ public class ChessMoveNotion {
     private Class<? extends Piece> pieceClass;
     private boolean shouldTakePiece;
     private boolean shouldEndInCheck;
+    private int pieceLocationColumn;
+    private int pieceLocationRow;
+    private boolean pawnPromotion;
+    private Class<? extends Piece> pawnPromotionPieceType;
     private int row;
     private int column;
 
-//    public ChessMoveNotion(String pieceColour, TypeOfMoveEnum typeOfMove, boolean shouldEndInCheck, int row, int column) {
-//        this.pieceColour = pieceColour;
-//        this.typeOfMove = typeOfMove;
-//        this.shouldEndInCheck = shouldEndInCheck;
-//        this.row = row;
-//        this.column = column;
-//    }
+    public ChessMoveNotion(String pieceColour, Class<? extends Piece> pieceClass, boolean shouldTakePiece, boolean shouldEndInCheck, int pieceLocationColumn, int pieceLocationRow, boolean pawnPromotion, Class<? extends Piece> pawnPromotionPieceType, int row, int column) {
+        this.pieceColour = pieceColour;
+        this.pieceClass = pieceClass;
+        this.shouldTakePiece = shouldTakePiece;
+        this.shouldEndInCheck = shouldEndInCheck;
+        this.pieceLocationColumn = pieceLocationColumn;
+        this.pieceLocationRow = pieceLocationRow;
+        this.pawnPromotion = pawnPromotion;
+        this.pawnPromotionPieceType = pawnPromotionPieceType;
+        this.row = row;
+        this.column = column;
+    }
+
+    public Class<? extends Piece> getPawnPromotionPieceType() {
+        return pawnPromotionPieceType;
+    }
+
+    public void setPawnPromotionPieceType(Class<? extends Piece> pawnPromotionPieceType) {
+        this.pawnPromotionPieceType = pawnPromotionPieceType;
+    }
+
+    public boolean isPawnPromotion() {
+        return pawnPromotion;
+    }
+
+    public void setPawnPromotion(boolean pawnPromotion) {
+        this.pawnPromotion = pawnPromotion;
+    }
+
+    public int getPieceLocationRow() {
+        return pieceLocationRow;
+    }
+
+    public void setPieceLocationRow(int pieceLocationRow) {
+        this.pieceLocationRow = pieceLocationRow;
+    }
+
+    public int getPieceLocationColumn() {
+        return pieceLocationColumn;
+    }
+
+    public void setPieceLocationColumn(int pieceLocationColumn) {
+        this.pieceLocationColumn = pieceLocationColumn;
+    }
+
+    public Class<? extends Piece> getPieceClass() {
+        return pieceClass;
+    }
+
+    public void setPieceClass(Class<? extends Piece> pieceClass) {
+        this.pieceClass = pieceClass;
+    }
+
+    public boolean isShouldTakePiece() {
+        return shouldTakePiece;
+    }
+
+    public void setShouldTakePiece(boolean shouldTakePiece) {
+        this.shouldTakePiece = shouldTakePiece;
+    }
 
     public boolean isShouldEndInCheck() {
         return shouldEndInCheck;

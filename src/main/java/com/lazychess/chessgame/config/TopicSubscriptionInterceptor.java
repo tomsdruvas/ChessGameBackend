c0ea08b51f3a2774dd49af48b3daa3a51310d86d
Toms Druvaskalns <93099357+tomsdruvas@users.noreply.github.com>
2023-09-24 22:45:00 +0100

    added websocket security

---
+++ src/main/java/com/lazychess/chessgame/config/TopicSubscriptionInterceptor.java
@@ -0,0 +1,37 @@
+package com.lazychess.chessgame.config;
+
+import java.util.Objects;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+import com.lazychess.chessgame.repository.entity.BoardDao;
+import com.lazychess.chessgame.service.BoardService;
+
+@Component
+public class TopicSubscriptionInterceptor implements ChannelInterceptor {
+
+    private final BoardService boardService;
+
+    public TopicSubscriptionInterceptor(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
+
+        if (StompCommand.SUBSCRIBE.equals(headerAccessor.getCommand())) {
+            String user = Objects.requireNonNull(headerAccessor.getUser()).getName();
+            String chessGameId = Objects.requireNonNull(message.getHeaders().get("simpDestination")).toString().replace("/topic/game-progress/", "");
+            BoardDao boardDao = boardService.findChessGameById(chessGameId);
+
+            boardService.checkIfPlayerIsPartOfThisGame(boardDao, user);
+        }
+        return message;
+    }
+}
